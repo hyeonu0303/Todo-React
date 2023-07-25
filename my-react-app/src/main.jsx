0a6b0b04e2dcde8ba25a36a4801ec9048aa0758d
Parkhyeonwoo <hyeonu030334@gmail.com>
2023-07-25 22:16:21 +0900

    router 셋팅

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,10 +1,23 @@
+/* eslint-disable */
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import RouterInfo from './router/router.jsx';
 import './index.css'
+
+
+//react-router-dom
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const RouterObject = createBrowserRouter(RouterInfo) 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={RouterObject}>
+      <App />
+    </RouterProvider>
   </React.StrictMode>,
 )
